2d8714863a6ecea9be17ce6f0c96391ad19b921a
ahmed7761 <sharif7761@gmail.com>
2022-08-02 23:58:31 +0600

    summary card design

--- src/components/products/ProductSummaryCard.vue
+++ src/components/products/ProductSummaryCard.vue
@@ -19,3 +19,38 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.card{
+  width: 80%;
+  margin: 10%;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: white;
+  box-shadow: 0 0 5px gray;
+  h5.price{
+    color: gray;
+  }
+  p.description{
+    font-size: .05rem;
+  }
+  p.text-muted{
+    color: gray;
+  }
+  button.view-product-button{
+    padding: 10px;
+    background-color: rgb(79, 160, 187);
+    border: none;
+    color: white;
+    font-weight: bold;
+    font-size: 1.15rem;
+    cursor: pointer;
+  }
+}
+@media (min-width: 500px) {
+  .card{
+    width: 350px;
+    margin: 10px;
+  }
+}
+</style> 
