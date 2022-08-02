9993c3c5cb8f318d015cb5c1cfdd87ff7aef1439
ahmed7761 <sharif7761@gmail.com>
2022-08-03 00:32:12 +0600

    cart item card component design

---
+++ src/components/cart/CartItemCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="cart-item-card">
+    <div class="header">
+      <h3>{{ product.name }}</h3>
+      <h4>In Cart: {{ product.quantity }}</h4>
+      <h4>Total Cost: {{ item_cost.toFixed(2) }}</h4>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'CartItemCard',
+  props: ['product'],
+  computed: {
+    item_cost(){
+      return this.product.price * this.product.quantity
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+.cart-item-card{
+  width: 90%;
+  margin: 5%;
+  background-color: white;
+  box-shadow: 0 0 5px gray;
+  padding: 10px;
+  text-align: left;
+
+  .header{
+    display: flex;
+    justify-content: space-around;
+  }
+}
+
+</style>
