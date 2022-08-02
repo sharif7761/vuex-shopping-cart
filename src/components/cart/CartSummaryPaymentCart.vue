9c6d1ceeef6947289900bcc7d73bf1356bba87af
ahmed7761 <sharif7761@gmail.com>
2022-08-03 00:34:00 +0600

    Cart summary component design

---
+++ src/components/cart/CartSummaryPaymentCart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="cart-item-card">
+    <h3>Cart Total: ${{ cart_total.toFixed(2)}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartSummaryPaymentCard',
+  computed: {
+    cart_total(){
+      return this.$store.getters.cartTotal;
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+.cart-item-card{
+  width: 90%;
+  margin: 5%;
+  background-color: white;
+  box-shadow: 0 0 5px gray;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
